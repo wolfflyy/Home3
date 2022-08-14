f421ce85b2b182a42fd8ac54c95608a7d1f5f9e3
wolfflyy <ducanh11022001@gmail.com>
2022-08-14 23:14:12 +0700

    hey hey people

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -33,4 +36,12 @@ class LoginController extends AbstractController
              'error' => $error,
           ]);
       }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+            return new RedirectResponse($targetPath);
+        }
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+    }
 }
